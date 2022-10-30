676b7d715fbb25dc27e9f5ea84d0fb0aabf031f0
Crocodile <wwwblogger1@gmail.com>
2022-10-30 20:08:15 +0200

    add Book_T_D_Chapter_3_example_1/

---
+++ Book_T_D_Chapter_3_example_1/Program.cs
@@ -0,0 +1,32 @@
+﻿
+public class Program
+{
+
+    static void LocalVarDeclarations()
+    {
+        Console.WriteLine("=> Data Declarations:");
+
+        // локальная переменная иициализируются так:
+        // типДанных имяПеременной = начальноеЗначение
+
+        int myInt = 0;
+
+        // Объявлять и присваивать можно также в двух отдельных строках
+
+        string myString;
+        myString = "This is my character data";
+
+        // Объявить три переменных типа bool в одной строке
+        bool b1 = true, b2 = false, b3 = b1;
+
+        // Использовать тип данных System.Boolean для объявления булевской переменной
+        System.Boolean b4 = false;
+
+
+        Console.WriteLine("Your data: {0}, {1}, {2}, {3}, {4}, {5}", myInt, myString, b1, b2, b3, b4);
+
+        Console.WriteLine();
+
+    }
+
+} 
