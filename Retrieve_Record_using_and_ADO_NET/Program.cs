afe2f1c20e91ce32b4c86a3ae6b0bc5d4293940a
Crocodile <wwwblogger1@gmail.com>
2023-02-08 19:06:16 +0200

    first commit

---
+++ Retrieve_Record_using_and_ADO_NET/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Data.SqlClient;
+namespace AdoNetConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Program().DisplayData();
+            Console.ReadKey();
+        }
+        public void DisplayData()
+        {
+            SqlConnection con = null;
+            try
+            {
+                // Creating Connection  
+                con = new SqlConnection("data source=; database=;integrated security=SSPI");
+
+                // writing sql query  
+                SqlCommand cm = new SqlCommand("Select * from student", con);
+
+                // Opening Connection  
+                con.Open();
+
+                // Executing the SQL query  
+                SqlDataReader sdr = cm.ExecuteReader();
+
+                // Iterating Data  
+                while (sdr.Read())
+                {
+                    // Displaying Record  
+                    Console.WriteLine(sdr["id"] + " " + sdr["name"] + " " + sdr["email"]);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("OOPs, something went wrong." + e);
+            }
+            // Closing the connection  
+            finally
+            {
+                con.Close();
+            }
+        }
+    }
+}
